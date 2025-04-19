a2641622ef69a2f8b11b6c08a44fb8625a293728
A-Wu5 <roboanthony@gmail.com>
2025-04-19 15:14:33 -0700

    Incorporated data and added a graph component

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
-from dash import Dash, html
+# Import packages
+from dash import Dash, html, dash_table, dcc
+import pandas as pd
+import plotly.express as px
 
+# Incorporate data
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv')
+
+# Initialize the app
 app = Dash()
 
-# Requires Dash 2.17.0 or later
-app.layout = [html.Div(children='Hello World')]
+# App layout
+app.layout = [
+    html.Div(children='My First App with Data and a Graph'),
+    dash_table.DataTable(data=df.to_dict('records'), page_size=10),
+    dcc.Graph(figure=px.histogram(df, x='continent', y='lifeExp', histfunc='avg'))
+]
 
+# Run the app
 if __name__ == '__main__':
     app.run(debug=True)
